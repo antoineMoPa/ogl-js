99d2b2b64bb51cb7f164954d1af073273cc72466
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-02-10 18:58:46 -0500

    oops + new window

--- ogl/main.cpp
+++ ogl/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include <GL/glut.h>
 
-int main(){
+GLvoid Render(){
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    
+}
+
+GLvoid Resize(GLint width,GLint height){
+    
+}
+
+int main(int argc, char **argv){
     std::cout << "Hey!" << std::endl;
+    
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+
+    glutCreateWindow("Hey");
+    
+    glutDisplayFunc(Render);
+    glutReshapeFunc(Resize);
+    
+
+    
+    glutMainLoop();
+    return 0;
 }
