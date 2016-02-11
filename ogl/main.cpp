965fbf773f039e6fe0920aa8d239252954a09b7b
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-02-11 09:53:12 -0500

    ignore binary + organize in a class

--- ogl/main.cpp
+++ ogl/main.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
 #include <GL/glut.h>
 
-GLvoid Render(){
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+class OglApp{
+public:
+    OglApp(int * argc, char ** argv){
+        glutInit(argc,argv);
+        glClearColor(0.0f,0.0f,0.0f,0.0f);
+        glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+        glutInitWindowSize(600,600);
+        glutReshapeFunc(Resize);
+        glutCreateWindow("Hey");
+        
+        
+        glutDisplayFunc(Render);
+        glutIdleFunc(Render);
+        
+        glutMainLoop();
+    }
+    static GLvoid Render(){
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+        glutSolidTeapot(30.0);
+        glDisable(GL_DEPTH_TEST);
+        
+        glLoadIdentity();
+        
+        glBegin(GL_TRIANGLES);
+        glVertex3f(-2.0f,-2.0f, 0.0f);
+        glVertex3f( 2.0f, 0.0f, 0.0);
+        glVertex3f( 0.0f, 2.0f, 0.0);
+        glEnd();
+        
+        glFlush();
+        glutSwapBuffers();
+    }
     
-}
-
-GLvoid Resize(GLint width,GLint height){
-    
-}
+    static GLvoid Resize(GLint width,GLint height){
+        
+    }
+};
 
 int main(int argc, char **argv){
-    std::cout << "Hey!" << std::endl;
+    OglApp app(&argc,argv);
     
-    glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
-
-    glutCreateWindow("Hey");
-    
-    glutDisplayFunc(Render);
-    glutReshapeFunc(Resize);
-    
-
-    
-    glutMainLoop();
     return 0;
 }
