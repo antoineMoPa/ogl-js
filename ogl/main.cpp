cd6c73ee6a1680f3d88dbecc5f2e802632ae8435
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-02-12 13:18:53 -0500

    with glclear now

--- ogl/main.cpp
+++ ogl/main.cpp
@@ -28,7 +28,7 @@ public:
         };
         
         auto Render = [](){
-            //glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
             
             
             Settings::i++;
