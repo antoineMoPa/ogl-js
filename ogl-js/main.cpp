89b8f73d83fddf50ae088351c011591c09b579f6
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-02-13 23:05:03 -0500

    OpenGL + JS

---
+++ ogl-js/main.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include "jsapi.h"
+#include <GL/glut.h>
+#include <cstdio>
+#include "math.h"
+
+/* The class of the global object. */
+static JSClass global_class = {
+    "global",
+    JSCLASS_GLOBAL_FLAGS,
+    JS_PropertyStub,
+    JS_DeletePropertyStub,
+    JS_PropertyStub,
+    JS_StrictPropertyStub,
+    JS_EnumerateStub,
+    JS_ResolveStub,
+    JS_ConvertStub
+};
+
+using namespace std;
+
+static JSBool jsfn_plus(JSContext *cx, unsigned argc, jsval *vp)
+{
+    JS::CallArgs args = CallArgsFromVp(argc, vp);
+    args.rval().setInt32(args[0].toInt32() + args[1].toInt32());
+    return true;
+}
+
+static JSBool jsfn_divide(JSContext *cx, unsigned argc, jsval *vp)
+{
+    JS::CallArgs args = CallArgsFromVp(argc, vp);
+    args.rval().setInt32(args[0].toInt32() / args[1].toInt32());
+    return true;
+}
+
+static JSBool jsfn_log(JSContext *cx, unsigned argc, jsval *vp)
+{
+    JS::CallArgs args = CallArgsFromVp(argc, vp);
+    const char * str = JS_EncodeString(cx,args[0].toString());
+    cout << str << endl;
+    return true;
+}
+
+class Settings{
+public:
+    static int w;
+    static int h;
+    static int i;
+};
+
+int Settings::w = 0;
+int Settings::h = 0;
+int Settings::i = 0;
+
+class OglApp{
+public:
+    OglApp(int * argc, char ** argv){
+        glutInit(argc,argv);
+        glClearColor(0.0f,0.0f,0.0f,0.0f);
+        glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+        glutInitWindowSize(Settings::w,Settings::h);
+        
+        auto Resize = [](int w,int h){
+            
+        };
+        
+        auto Render = [](){
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+            
+            glLoadIdentity();
+            gluPerspective(
+                           80,
+                           float(Settings::w)/float(Settings::h),
+                           1,
+                           100
+                           );
+
+            gluLookAt(0.0f,0.0f,2.0f,
+                      0.0f,0.0f,0.0f,
+                      0.0f,1.0f,0.0f);
+            
+            glTranslatef(-0.4,-1,0);
+            glScalef(0.1,0.1,0.1);
+
+            glColor3f(0.6,0.3,1);
+            
+            glBegin(GL_TRIANGLE_STRIP);
+            glVertex3f(0.0f,1.0f,0.0f);
+            glVertex3f(1.0f,1.0f,0.0);
+            glVertex3f(1.0f,0.0f,0.0);
+            glVertex3f(3.0f,1.0f,0.0);
+            glVertex3f(3.0f,0.0f,0.0);
+            glVertex3f(4.0f,1.0f,0.0);
+            glEnd();
+                
+            glFlush();
+            glutSwapBuffers();
+        };
+        
+        glutReshapeFunc(Resize);
+        glutCreateWindow("Hey");
+        
+        glutDisplayFunc(Render);
+        glutIdleFunc(Render);
+       
+        glutMainLoop();
+    }
+private:
+    int i = 0;
+};
+
+int main(int argc, char ** argv)
+{
+    JSRuntime *rt = JS_NewRuntime(8L * 1024 * 1024, JS_USE_HELPER_THREADS);
+    if (!rt)
+        return 1;
+
+        
+    JSContext *cx = JS_NewContext(rt, 8192);
+    if (!cx)
+        return 1;
+
+    {
+        // Scope for our various stack objects
+        // (JSAutoRequest, RootedObject), so they all go
+        // out of scope before we JS_DestroyContext.
+
+        JSAutoRequest ar(cx);
+        // In practice, you would want to exit this any
+        // time you're spinning the event loop
+        
+        JS::RootedObject global(cx, JS_NewGlobalObject(cx, &global_class, nullptr));
+        
+        if (!global)
+            return 1;
+        
+        JS::RootedValue rval(cx);
+
+        // Scope for JSAutoCompartment
+        { 
+            JSAutoCompartment ac(cx, global);
+            JS_InitStandardClasses(cx, global);
+
+            static JSFunctionSpec my_functions[] = {
+                JS_FN("plus", jsfn_plus, 2, 0),
+                JS_FN("divide", jsfn_divide, 2, 0),
+                JS_FN("log", jsfn_log, 1, 0),
+                JS_FS_END
+            };
+            
+            bool ok = JS_DefineFunctions(cx, global, my_functions);            
+            if(!ok){
+                cout << "Function definition error" << endl;
+                return 1;
+            }
+            const char *script =
+                "log('meow')";
+            const char *filename = "noname";
+            int lineno = 1;
+
+            ok = JS_EvaluateScript(cx, global, script, strlen(script), filename, lineno, rval.address());
+
+            if(!ok)
+                return 1;
+        }
+        if(rval.isString()){
+            JSString *str = rval.toString();
+            char * str2 = JS_EncodeString(cx, str);
+            printf("%s\n", str2);
+        }
+    }
+    
+    JS_DestroyContext(cx);
+    JS_DestroyRuntime(rt);
+    JS_ShutDown();
+
+    Settings::w = 640;
+    Settings::h = 480;
+    OglApp app(&argc,argv);
+    
+    return 0;
+}
