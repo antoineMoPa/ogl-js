4369abde99c003b22fe2dc78b9ceaad4c231c685
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-03-13 21:40:21 -0400

    prepare for materials

--- ogl-js/OglApp/Model.h
+++ ogl-js/OglApp/Model.h
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include "math.h"
 
-namespace OglApp{    
+namespace OglApp{
     using namespace std;
 
     typedef array<float,3> vec3;
@@ -13,7 +13,7 @@ namespace OglApp{
     /*
       Override >> operators for vectors so we can scan
       a file easily:
-      
+
       file >> v3;
      */
     istream & operator>>(istream & is, vec3 & v3){
@@ -25,10 +25,13 @@ namespace OglApp{
         is >> v2[0] >> v2[1];
         return is;
     }
-    
+
     class Material{
         friend class MaterialLib;
     public:
+        void bind(){
+
+        }
         ~Material(){
             if(ka_img != nullptr)
                 delete ka_img;
@@ -58,12 +61,12 @@ namespace OglApp{
                 }
             }
         }
-        
+
     private:
         vec3 ka;
         vec3 kd;
         vec3 ks;
-        string ka_path;        
+        string ka_path;
         string kd_path;
         string ks_path;
         Image * ka_img = nullptr;
@@ -72,7 +75,7 @@ namespace OglApp{
     };
 
     using MaterialMap = map<string,Material>;
-    
+
     class MaterialLib{
     private:
         MaterialMap materials;
@@ -85,16 +88,16 @@ namespace OglApp{
             if(!file){
                 return;
             }
-            
+
             Material * current_material = nullptr;
-            
+
             while(!file.eof()){
                 file >> s;
                 if(s.substr(0,6) == "newmtl"){
                     if(current_material != nullptr){
                         current_material->load_images();
                     }
-                    
+
                     string index;
                     file >> index;
                     current_material = &materials[index];
@@ -112,9 +115,7 @@ namespace OglApp{
                     file >> current_material->ka_path;
                 }
                 else if(s.substr(0,6) == "map_Kd"){
-                    cout << "meow" << endl;
                     file >> current_material->kd_path;
-                    cout << "path: " << current_material->kd_path << endl;
                 }
                 else if(s.substr(0,6) == "map_Ks"){
                     file >> current_material->ks_path;
@@ -123,145 +124,24 @@ namespace OglApp{
                     getline(file,s);
                 }
             }
-            
+
             file.close();
         }
     };
-        
-    /**
-       Code + shameless inspiration
-       http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
-       
-       The code of this class has to be seriously reviewed if user input is not
-       trusted.
-    */
-    class Model{
+
+    class ModelPart{
+        friend class Model;
     public:
-        void load(const char * filename){
-            vertices.clear();
-            uvs.clear();
-            normals.clear();
-            faces3.clear();
-            
-            ifstream file;
-            char c;
-            string s;
-            // temp vars
-            float x;
-            float y;
-            float z;
-            vec3 v3;
-            vec2 v2;
-            face3 f3;
-            string face;
-            vector<int> tempfaceint;
-            
-            file.open(filename);
-            
-            if(!file.is_open()){
-                return;
-            }
-            
-            while(!file.eof()){
-                // first part of string
-                file >> s;
-                if(s.substr(0,2) == "vt"){
-                    file >> v2;
-                    uvs.push_back(v2);
-                }
-                else if(s.substr(0,2) == "vn"){
-                    file >> v3;
-                    normals.push_back(v3);
-                }
-                else if(s.substr(0,1) == "v"){
-                    file >> v3;
-                    vertices.push_back(v3);
-                }
-                else if(s.substr(0,1) == "f"){
-                    tempfaceint.clear();
-                    // skip character
-                    file.get();
-                    getline(file,face);
-                    // http://stackoverflow.com/questions/2896600
-                    replace(face.begin(),face.end(),'/',' ');
-                    stringstream f(face);
-                    int tempint;
-                    while(f){
-                        f >> tempint;
-                        tempfaceint.push_back(tempint);
-                    }
-                    // remove last read number
-                    tempfaceint.pop_back();
-                    
-                    if(tempfaceint.size() == 12){
-                        cout << "Please triangulate your model." << endl;
-                    }
-                    if(tempfaceint.size() == 9){
-                        for(vector<int>::iterator it = tempfaceint.begin();
-                            it!= tempfaceint.end();
-                            ){
-                            for(int i = 0; i < 9; i++,++it){
-                                f3[i] = *it;
-                            }
-                            faces3.push_back(f3);
-                        }
-                    } else if (tempfaceint.size() == 6){
-                        // Todo: this is so wrong
-                        // We should verify if we
-                        // have normals or uvs
-                        for(vector<int>::iterator it = tempfaceint.begin();
-                            it!= tempfaceint.end();
-                            ){
-                            for(int i = 0; i < 9; i++){
-                                if(i % 3 == 0){
-                                    f3[i] = *it;
-                                    ++it;
-                                    ++it;
-                                    // TODO: verify vertex actually exists
-                                } else {
-                                    f3[i] = 0;
-                                }
-                            }
-                            faces3.push_back(f3);
-                        }
-                    } else if (tempfaceint.size() == 3){
-                        for(vector<int>::iterator it = tempfaceint.begin();
-                            it!= tempfaceint.end();
-                            ){
-                            for(int i = 0; i < 9; i++){
-                                if(i % 3 == 0){
-                                    f3[i] = *it;
-                                    ++it;
-                                    // TODO: verify vertex actually exists
-                                } else {
-                                    f3[i] = 0;
-                                }
-                            }
-                            faces3.push_back(f3);
-                        }
-                    }
-                } else if (s.substr(0,6) == "mtllib"){
-                    string path;
-                    file >> path;
-                    materials.load(path);
-                }
-                else{
-                    getline(file,s);
-                }
-            }
-            file.close();
+        ModelPart(){
+            // nothing yet
         }
-        
         void create_buffers(){
-            if(buffers_created){
-                return;
-            }
             size_t numpoints = faces3.size() * 3;
             vertex_num = numpoints;
             vertex_buffer_data = new GLfloat[numpoints * 3];
             normal_buffer_data = new GLfloat[numpoints * 3];
             uv_buffer_data = new GLfloat[numpoints * 2];
-            
+
             typedef vector<face3>::iterator faceit;
 
             int i = 0;
@@ -304,7 +184,7 @@ namespace OglApp{
                     uv_buffer_data[uvi+4] = uvs[(*it)[7]-1][0];
                     uv_buffer_data[uvi+5] = uvs[(*it)[7]-1][1];
                 }
-                
+
                 i += 9;
                 uvi += 6;
             }
@@ -324,7 +204,7 @@ namespace OglApp{
                 sizeof(GLfloat) * numpoints * 2,
                 uv_buffer_data,
                 GL_STATIC_DRAW);
-            
+
             glGenBuffers(1, &normal_buffer);
             glBindBuffer(GL_ARRAY_BUFFER, normal_buffer);
             glBufferData(
@@ -332,13 +212,9 @@ namespace OglApp{
                 sizeof(GLfloat) * numpoints * 3,
                 normal_buffer_data,
                 GL_STATIC_DRAW);
-
-            buffers_created = true;
         }
+
         void render(){
-            if(!buffers_created){
-                return;
-            }
             // Vertex data
             glEnableVertexAttribArray(0);
             glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
@@ -350,10 +226,10 @@ namespace OglApp{
                 0, // stride
                 (void*)0 // array buffer offset
                 );
-            
+
             glDrawArrays(GL_TRIANGLES, 0, vertex_num);
             glDisableVertexAttribArray(0);
-            
+
             // UV data
             glEnableVertexAttribArray(1);
             glBindBuffer(GL_ARRAY_BUFFER, uv_buffer);
@@ -378,14 +254,15 @@ namespace OglApp{
                 (void*)0 // array buffer offset
                 );
         }
-        ~Model(){
-            if(vertex_buffer_data == NULL){
+
+        ~ModelPart(){
+            if(vertex_buffer_data != NULL){
                 delete vertex_buffer_data;
             }
-            if(normal_buffer_data == NULL){
+            if(normal_buffer_data != NULL){
                 delete normal_buffer_data;
             }
-            if(uv_buffer_data == NULL){
+            if(uv_buffer_data != NULL){
                 delete uv_buffer_data;
             }
         }
@@ -395,7 +272,7 @@ namespace OglApp{
         GLfloat * uv_buffer_data = NULL;
 
     private:
-        bool buffers_created = false;
+        string material;
         vector <vec3> vertices;
         vector <vec2> uvs;
         vector <vec3> normals;
@@ -404,6 +281,169 @@ namespace OglApp{
         GLuint vertex_buffer;
         GLuint normal_buffer;
         GLuint uv_buffer;
+
+    };
+
+    /**
+       Code + shameless inspiration
+       http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
+
+       The code of this class has to be seriously reviewed if user input is not
+       trusted.
+    */
+    class Model{
+    public:
+        void load(const char * filename){
+            ModelPart * curr = new ModelPart();
+            parts.push_back(curr);
+            curr->vertices.clear();
+            curr->uvs.clear();
+            curr->normals.clear();
+            curr->faces3.clear();
+
+            ifstream file;
+            char c;
+            string s;
+            // temp vars
+            float x;
+            float y;
+            float z;
+            vec3 v3;
+            vec2 v2;
+            face3 f3;
+            string face;
+            vector<int> tempfaceint;
+
+            file.open(filename);
+
+            if(!file.is_open()){
+                return;
+            }
+
+            while(!file.eof()){
+                // first part of string
+                file >> s;
+                if(s.substr(0,2) == "vt"){
+                    file >> v2;
+                    curr->uvs.push_back(v2);
+                }
+                else if(s.substr(0,2) == "vn"){
+                    file >> v3;
+                    curr->normals.push_back(v3);
+                }
+                else if(s.substr(0,1) == "v"){
+                    file >> v3;
+                    curr->vertices.push_back(v3);
+                }
+                else if(s.substr(0,1) == "f"){
+                    tempfaceint.clear();
+                    // skip character
+                    file.get();
+                    getline(file,face);
+                    // http://stackoverflow.com/questions/2896600
+                    replace(face.begin(),face.end(),'/',' ');
+                    stringstream f(face);
+                    int tempint;
+                    while(f){
+                        f >> tempint;
+                        tempfaceint.push_back(tempint);
+                    }
+                    // remove last read number
+                    tempfaceint.pop_back();
+
+                    if(tempfaceint.size() == 12){
+                        cout << "Please triangulate your model." << endl;
+                    }
+                    if(tempfaceint.size() == 9){
+                        for(vector<int>::iterator it = tempfaceint.begin();
+                            it!= tempfaceint.end();
+                            ){
+                            for(int i = 0; i < 9; i++,++it){
+                                f3[i] = *it;
+                            }
+                            curr->faces3.push_back(f3);
+                        }
+                    } else if (tempfaceint.size() == 6){
+                        // Todo: this is so wrong
+                        // We should verify if we
+                        // have normals or uvs
+                        for(vector<int>::iterator it = tempfaceint.begin();
+                            it!= tempfaceint.end();
+                            ){
+                            for(int i = 0; i < 9; i++){
+                                if(i % 3 == 0){
+                                    f3[i] = *it;
+                                    ++it;
+                                    ++it;
+                                    // TODO: verify vertex actually exists
+                                } else {
+                                    f3[i] = 0;
+                                }
+                            }
+                            curr->faces3.push_back(f3);
+                        }
+                    } else if (tempfaceint.size() == 3){
+                        for(vector<int>::iterator it = tempfaceint.begin();
+                            it!= tempfaceint.end();
+                            ){
+                            for(int i = 0; i < 9; i++){
+                                if(i % 3 == 0){
+                                    f3[i] = *it;
+                                    ++it;
+                                    // TODO: verify vertex actually exists
+                                } else {
+                                    f3[i] = 0;
+                                }
+                            }
+                            curr->faces3.push_back(f3);
+                        }
+                    }
+                } else if (s.substr(0,6) == "mtllib"){
+                    string path;
+                    file >> path;
+                    materials.load(path);
+                }
+                else{
+                    getline(file,s);
+                }
+            }
+
+            file.close();
+        }
+
+        void create_buffers(){
+            if(buffers_created){
+                return;
+            }
+            using it_type = vector<ModelPart*>::iterator;
+
+            for(it_type it = parts.begin(); it != parts.end();++it){
+                (*it)->create_buffers();
+            }
+
+            buffers_created = true;
+        }
+
+        void render(){
+            if(!buffers_created){
+                return;
+            }
+
+            using it_type = vector<ModelPart*>::iterator;
+
+            for(it_type it = parts.begin(); it != parts.end();++it){
+                (*it)->render();
+            }
+        }
+        ~Model(){
+            using it_type = vector<ModelPart*>::iterator;
+            for(it_type it = parts.begin(); it != parts.end();++it){
+                delete *it;
+            }
+        }
+    private:
+        bool buffers_created = false;
         MaterialLib materials;
+        vector<ModelPart*> parts;
     };
 }
