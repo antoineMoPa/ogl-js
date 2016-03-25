e65de4e23c657e4aced87b0b286c4503411a9959
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-03-24 22:10:49 -0400

    fix interference with i

--- jslib/main.js
+++ jslib/main.js
@@ -2,7 +2,7 @@ var render;
 
 var console = {};
 console.log = function(){
-    for(i in arguments){
+    for(var i in arguments){
         var arg = arguments[i];
         if(typeof arg == 'object'){
             log(JSON.stringify(arg));
