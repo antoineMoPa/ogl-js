2ca729b6bdd911419276da0c0b42e313e395cd01
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-04-06 18:56:51 -0400

    prepare new test

--- tests/vbo/main.js
+++ tests/vbo/main.js
@@ -77,42 +77,83 @@ create_triangle_array(
     ]
 );
 
+xy_xz_plane_vert = [
+    0,0,0,
+    1,0,0,
+    0,1,0,
+    0,1,0,
+    1,0,0,
+    1,1,0,
+    
+    0,0,0,
+    1,0,0,
+    0,0,1,
+    0,0,1,
+    1,0,0,
+    1,0,1
+];
+
+xy_xz_plane_normal = [
+    0,1,0,
+    0,1,0,
+    0,1,0,
+    0,1,0,
+    0,1,0,
+    0,1,0
+];
+
+xy_xz_plane_uv = [
+    0,0,
+    0,0,
+    0,0,
+    0,0,
+    0,0,
+    0,0
+];
+
 create_triangle_array(
     "xyplane+xzplane",
-    [
-        0,0,0,
-        1,0,0,
-        0,1,0,
-        0,1,0,
-        1,0,0,
-        1,1,0,
-
-        0,0,0,
-        1,0,0,
-        0,0,1,
-        0,0,1,
-        1,0,0,
-        1,0,1
-    ],
-    [
-        0,1,0,
-        0,1,0,
-        0,1,0,
-        0,1,0,
-        0,1,0,
-        0,1,0
-    ],
-    [
-        0,0,
-        0,0,
-        0,0,
-        0,0,
-        0,0,
-        0,0
-    ]
+    xy_xz_plane_vert,
+    xy_xz_plane_normal,
+    xy_xz_plane_uv
 );
 
+/*
+  Multiply matrices of 3d coords 
+  multiply_matrix_3d(mat1,mat2) =
+  [a,b,c,a2,b2,c2,a3,b3,c3] * [x,y,z,x2,y2,z2,...]
+ */
+function multiply_matrix_3d(mat1,mat2){
+    var num;
+    for(var i = 0; i < mat2.length / 3; i++){
+        for(var j = 0; j < 3; j++){
+            // initialize
+            num = 0;
+            for(var k = 0; k < 3; k++){
+                num += mat1[3*j + k] * mat2[3*i+k];
+            }
+            mat2[3 * i + j] = num;
+        }
+    }
+}
 
+function rotate_y(theta,mat){
+    // Build a rotation matrix
+    var r = [
+        Math.cos(theta),0,Math.sin(theta),
+        0,1,0,
+        (-Math.sin(theta)),0,Math.cos(theta)
+    ];
+    // Rotate
+    multiply_matrix_3d(r,mat);
+}
+
+create_triangle_array(
+    "xyplane+xzplane_rotated",
+    xy_xz_plane_vert,
+    xy_xz_plane_normal,
+    xy_xz_plane_uv
+);
 
 var it = 0;
 
@@ -120,16 +161,20 @@ function render(){
     bind_shaders("main");
     it++;
 
-    translate(-2,-2 + it/100,-2);
+    translate(-3,-2 + it/100,-2);
     
     render_triangle_array("xzplane");
     
     translate(0,0,-0.1);
     
     render_triangle_array("xyplane");
-
+    
     translate(2,0,0);
     
     render_triangle_array("xyplane+xzplane");
 
+    translate(2,0,0);
+
+    render_triangle_array("xyplane+xzplane_rotated");
+    
 }
