2c60b022d681154cc856b9d0561e3d016b4a6df9
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-04-16 12:11:37 -0400

    blur for fun

--- tests/roads/post-fragment.glsl
+++ tests/roads/post-fragment.glsl
@@ -15,4 +15,35 @@ void main(){
     // Distance from center:
     highp float dist_from_center = distance(vec2(0.5,0.5),uv);
     color *= 1.0 - pow(dist_from_center,2.0);
+
+    highp vec2 coord;
+    highp float dist_fac;
+    // Above 3 is too much for my poor little chromebook
+
+    int radius = 3;
+    highp float pixel_size = 0.002;
+    for(int i = -radius; i < radius; i++){
+        for(int j = -radius; j < radius; j++){
+            coord = uv;
+            coord += vec2(pixel_size*float(i),
+                          pixel_size*float(j));
+            
+            if(coord.x > 1.0 || coord.y > 1.0 ||
+               coord.x < 0.0 || coord.y < 0.0){
+                continue;
+            }
+
+            dist_fac = sqrt(pow(i,2.0) + pow(j,2.0))/3.0;
+
+            if(dist_fac > 1.0){
+                continue;
+            }
+            
+            color += dist_fac * texture(
+                             renderedTexture,
+                             coord
+                             );
+        }
+    }
+    color /= pow(radius,2.0);
 }
