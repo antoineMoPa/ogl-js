d788dd480205ed62956a3e3ff445e47f1b1cd80c
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-04-29 20:06:18 -0400

    Add more comments to fragment shader

--- tests/fluid/main.js
+++ tests/fluid/main.js
@@ -2,14 +2,41 @@
 enable_2_pass_pp();
 
 var mouse = {x:0,y:0};
+var damp = 0;
+var wall = 0;
+var reset = 0;
+var pause = -1;
+var radius = 0.003;
 
 function render(){
     shader_var("mouse_x",mouse.x);
     shader_var("mouse_y",mouse.y);
+    shader_var("damp",damp);
+    shader_var("wall",wall);
+    shader_var("reset",reset);
+    shader_var("radius",radius);
+    shader_var("pause",pause);
+    damp *= 0.8;
+    reset = 0;
 }
 
 function on_key(key, x, y){
-
+    if(key == "d"){
+        damp+=0.1;
+    } else if(key == "w"){
+        wall++;
+        wall = wall % 2;
+        console.log("Wall: " + wall);
+        reset = 1;
+    } else if(key == "r"){
+        radius += 0.005;
+        console.log("Radius: " + radius);
+    } else if(key == "R"){
+        radius -= 0.005;
+        console.log("Radius: " + radius);
+    } else if(key == "p"){
+        pause *= -1;
+    }
 }
 
 function on_mouse_move(x, y){
--- tests/fluid/post-fragment.glsl
+++ tests/fluid/post-fragment.glsl
@@ -9,29 +9,57 @@ uniform sampler2D pass_2;
 uniform int time;
 uniform int pass;
 uniform int frame_count;
+uniform int wall;
+uniform int reset;
+uniform int pause;
 uniform highp float ratio;
+uniform highp float damp;
+uniform highp float radius;
 uniform highp float mouse_x;
 uniform highp float mouse_y;
 highp vec4 rand_var;
 
 void main(){
+    // We'll use this later
     highp vec4 last = texture(last_pass,UV);
 
+    // Height, speed and resistance is stored in a texture
+    // red = x = height
+    // green = y = speed
+    // etc.
+    // Since we can't store a negative value,
+    // values are given a 0.5 offset at storage
+    // (Also: we cannot store values > 1.0)
     highp vec4 data = texture(pass_2,UV);
     highp float height = data.x - 0.5;
     highp float speed = data.y - 0.5;
     highp float resistance = data.z;
 
-    if(distance(UV,vec2(mouse_x,mouse_y)) < 0.01){
+    // Ratio used to find distance between mouse and
+    // current position
+    highp vec2 uv_ratio = vec2(1.0,1.0 / ratio);
+
+    // Compute mouse distance
+    highp float mouse_dist =
+        distance(UV * uv_ratio,
+                 vec2(mouse_x,mouse_y) * uv_ratio);
+    
+    // Activate source if close enough to mouse
+    if(mouse_dist < radius){
         if(resistance < 0.5){
-            speed += 0.05 * sin(float(time%100000)/100.0);
+            // Give this a sine in function of time
+            height += 100.0 * sin(float(mod(time,10000))/100.0);
         }
     }
-
-    if(frame_count == 0){
-        // here we store height
+    
+    if(frame_count == 0 || reset == 1){
+        // Set initial conditions
         color = vec4(0.5,0.5,1.0,1.0);
+
+        // Border width
         highp float border = 0.01;
+
+        // Create border at window edges
         if(UV.x > border&&
            UV.x < 1.0 - border &&
            UV.y > border &&
@@ -40,49 +68,76 @@ void main(){
             // No resistance here
             color.z = 0.0;
         }
+
+        // Create wall
+        if(wall == 1){
+            // Double slit experiment
+            if(UV.x > 0.30 && UV.x < 0.31){
+                if(UV.y > 0.42 && UV.y < 0.46 ||
+                   UV.y > 0.58 && UV.y < 0.62
+                   ){
+                    color.z = 0.0;
+                } else {
+                    color.z = 1.0;
+                }
+            }
+        }
     } else if(pass == 1){
-        // Compute speed
-        highp float pixel_width = 0.001;
+        // In pause, just leave here
+        if(pause == 1){
+            color = texture(pass_1,UV);
+            return;
+        }
+
+        // Not the real pixel size
+        // (Can be anything)
+        highp float pixel_width = 0.003;
+        // Create these values to find neighboring cells
         highp vec2 x_offset = vec2(pixel_width,0.00);
         highp vec2 y_offset = vec2(0.00,pixel_width * ratio);
-        
-        highp float speed_fac = 0.24;
-        
-        speed -= speed_fac *
+
+        // Fluid transfer between neighboring cells
+        speed -= 0.25 *
             (
              height - (texture(pass_2,UV + x_offset).x - 0.5) +
              height - (texture(pass_2,UV - x_offset).x - 0.5) +
              height - (texture(pass_2,UV + y_offset).x - 0.5) +
              height - (texture(pass_2,UV - y_offset).x - 0.5)
              );
-        
+
+        // Modify height according to speed
+        // The factor is adjusted so that the
+        // simulation works.
+        // Weird things happen when this is to high
+        height += 0.2 * speed;
+
+        // Compute resistance to borders / walls / etc.
+        speed *= 1.0 - resistance;
+
         // Damp speed
-        speed *= 0.9999;
-                
+        // no damping = weird behaviour
+        // to much damping = you don't see anything
+        speed *= 0.998;
+        
+        // Damp when user uses the keyboard to damp stuff
+        height *= 1.0 - damp;
+
+        // We store data in the color
         color = vec4(
                      height + 0.5,
                      speed + 0.5,
                      resistance,
                      1.0
                      );
-
-    } else if(pass == 2){
-        color = last;
-        speed = color.y - 0.5;
-        height = color.x - 0.5;
-        resistance = color.z;
-        height += (0.3 * speed * (1.0 - resistance));
-
-        color = vec4(
-             height + 0.5,
-             speed + 0.5,
-             resistance,
-             1.0 );
         
+    } else if(pass == 2){
+        // We do nothing here
+        color = last;
     } else if(pass == 3){
         // Draw stuff
-        color = last;
-        color.rgb = vec3(color.x);
+        // last.x = height
+        // last.z = wall
+        color.rgb = vec3(last.x + last.z);
         color.a = 1.0;
     }
 }
