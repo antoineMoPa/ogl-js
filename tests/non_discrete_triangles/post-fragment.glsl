81b73e5a61ed6aa6905b0263721ebd3366b08f35
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-05-29 10:54:30 -0400

    Add some comments

--- tests/non_discrete_triangles/post-fragment.glsl
+++ tests/non_discrete_triangles/post-fragment.glsl
@@ -34,7 +34,9 @@ void main(){
     highp vec2 y_offset = vec2(0.00,pixel_heigth);
     
     if(frame_count == 0 || reset == 1){
+        // Initial screen color
         if(distance(UV,vec2(0.5,0.05)) < 3.0 * float(pixel_width)){
+            // Point at center bottom
             color = vec4(1.0,1.0,1.0,1.0);
         } else {
             color = vec4(0.0,0.0,0.0,1.0);
@@ -45,37 +47,31 @@ void main(){
                             (1.0 - UV.y) *
                             float(screen_h));
 
+        // Last pixel value
         last = texture(pass_2,UV);
 
-        if(last.x > 0.5){
-            //color = last;
-            //return;
-        }
-        
+        // When I type `p`
         if(pause == 1){
             color = last;
             return;
         }
             
-        if(!(this_line == active_line)){
-            //color = last;
-            //return;
-        }
-
         highp vec4 cell_1, cell_2, cell_3,
             cell_4, cell_5, cell_6;
-        
+
+        // Get other pixel values
         cell_1 = texture(pass_2,
-                         UV - 2.0 * y_offset - 1.0 * x_offset);
+                         UV - 2.0 * y_offset + 1.0 * x_offset);
         cell_2 = texture(pass_2,
-                         UV - 2.0 * y_offset);
+                         UV + 2.0 * y_offset);
         cell_3 = texture(pass_2,
-                         UV - 1.0 * y_offset + 1.0 * x_offset);
+                         UV + 1.0 * y_offset - 1.0 * x_offset);
 
         color = last;
 
         int num;
 
+        // Do weird stuff
         if (this_line % 30 != 0 || this_line % 20 < 4) {
             color.r += cell_1.r + cell_3.g * 0.1;
             color.g += cell_2.g;
@@ -86,6 +82,7 @@ void main(){
             color.b += cell_3.r;
         }
 
+        // Multiply bin sin
         color *= (sin(UV.y * 30.0 + color.r) + 1.4);
         color *= (sin(UV.x * 30.0 + color.g) + 1.4);
         
@@ -94,6 +91,7 @@ void main(){
         // We do nothing here
         color = last;
     } else if(pass == 3){
+        // We still do nothing
         color = last;
         color.a = 1.0;
     }
