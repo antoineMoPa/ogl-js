b50e1077fa9e8682617169ea4c45ae6a1bb2bc82
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-06-01 17:44:24 -0400

    image in post process example

--- tests/pp_textures/post-fragment.glsl
+++ tests/pp_textures/post-fragment.glsl
@@ -13,6 +13,25 @@ uniform int frame_count;
 highp vec4 rand_var;
 
 void main(){
-    color = texture(bg,UV);
+    highp vec2 mod_UV;
+    highp float time_fac = float(time % 10000) / 300.0;
+
+    highp float modulation = 0.1 *
+        sin(1.0 * UV.x + time_fac + 2.0 * UV.y);
+    
+    mod_UV.x = UV.x +
+        modulation;
+    mod_UV.y = UV.y;
+
+    mod_UV *= 1.5;
+    mod_UV -= vec2(0.25,0.25);
+
+    color = texture(bg, mod_UV);
+    
+    if( mod_UV.x < 0.0 || mod_UV.x > 1.0 ||
+        mod_UV.y < 0.0 || mod_UV.y > 1.0 ){
+        color.rgb = vec3(0.0,0.0,0.0);
+    }
+    
     color.a = 1.0;
 }
